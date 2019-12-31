9e3e0c967354774c23f7563266c9db3ba22a5904
Eduardo Moreira <contato@eduardoem.com.br>
2019-12-31 16:13:16 -0300

    Change port to .env

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+require('./src/config/database');
+
 const app = require('./src/config/server');
-const database = require('./src/config/database');
-const port = 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
 	console.log(`Online server on port ${port}`);
