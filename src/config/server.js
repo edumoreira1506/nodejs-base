bcaa9732fd4d0c6457b46d46877387f85791cb54
Eduardo Moreira <contato@eduardoem.com.br>
2020-01-02 21:25:54 -0300

    Add and install body-parser

--- src/config/server.js
+++ src/config/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static('./src/public'));
-
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 app.use(routes);
 
 app.use((_, res, next) => {
